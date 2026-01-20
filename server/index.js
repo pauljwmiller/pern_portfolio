fe4b4a6620b57a9395956ebd3d4a1a3290b9b5dd
Paul Miller <pauljwmiller@gmail.com>
2026-01-20 17:49:33 -0500

    Fix wildcard route for new Express version

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.post("/items", async(req, res) => {
 })
 
 // handles client-side routing
-app.get("*", (req, res) => {
+app.get("/(.*)", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
